91bbf580c8447d04a0d251a3f450936e07f4401e
Joseph <jbaugh78@msn.com>
2019-03-29 15:14:18 -0500

    basic get functioning for customer

--- BangazonAPI/Controllers/CustomersController.cs
+++ BangazonAPI/Controllers/CustomersController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonAPI.Controllers
 {
@@ -11,5 +14,55 @@ namespace BangazonAPI.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private readonly IConfiguration configuration;
+
+        public CustomersController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT c.id, c.firstname, c.lastname FROM Customer c";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Customer> customers = new List<Customer>();
+                    while (reader.Read())
+                    {
+                        Customer customer = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+                        customers.Add(customer);
+                    }
+
+                    reader.Close();
+                    if (customers.Count == 0)
+                    {
+                        return NoContent();
+                    }
+                    else
+                    {
+                        return Ok(customers);
+                    }
+                }
+            }
+        }
     }
 }
