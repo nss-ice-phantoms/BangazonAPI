e1f806d71a6bf063a09d527ca2a7e1afc5844c0c
Joseph <jbaugh78@msn.com>
2019-03-29 15:46:39 -0500

    apiclientprovider working basic get test for customer is passing

---
+++ TestBangazonAPI/CustomerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public async Task Test_GetAllCustomers()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/customers");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customerList = JsonConvert.DeserializeObject<List<Customer>>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(customerList.Count > 0);
+            }
+        }
+    }
+}
